4a4b3c59edec33f176d94bd01b103e833dbd4c41
Artur <artur-fedyuk@mail.ru>
2018-02-25 23:50:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     result = {};
-    const KEYS = ['H', 'Q', 'D', 'N', 'P'],
-          COINS = { 
-                H: 50,
-                Q: 25,
-                D: 10,
-                N: 5,
-                P: 1
-        };
+    if (!currency) return result;
+    if (currency > 10000) return 0;
+
+    num = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
+
     while (currency > 0) {
-        for (let a in config) {
-            if (config[a] <= currency) {
+        for (let a in num) {
+            if (num[a] <= currency) {
                 result[a] = result[a] || 0;
                 result[a]++;
-                currency -= config[a];
+                currency -= num[a];
                 break;
             }
         }
@@ -22,4 +18,3 @@ module.exports = function makeExchange(currency) {
 
     return result;
 };
-}
