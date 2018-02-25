2b5ed07a3778f5487116bffd766b2d809dc52a0e
Artur <artur-fedyuk@mail.ru>
2018-02-25 23:51:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 module.exports = function makeExchange(currency) {
     result = {};
-    if (!currency) return result;
-
     num = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
 
     while (currency > 0) {
