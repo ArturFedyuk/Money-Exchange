7285136004d0501fef3ac6d95d1963e13b32626c
Artur <artur-fedyuk@mail.ru>
2018-02-25 23:47:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    result = {};
+    const KEYS = ['H', 'Q', 'D', 'N', 'P'],
+          COINS = { 
+                H: 50,
+                Q: 25,
+                D: 10,
+                N: 5,
+                P: 1
+        };
+    while (currency > 0) {
+        for (let a in config) {
+            if (config[a] <= currency) {
+                result[a] = result[a] || 0;
+                result[a]++;
+                currency -= config[a];
+                break;
+            }
+        }
+    }
+
+    return result;
+};
 }
